a95b28b6f8a5ea928f22fe96310193ee665b130d
tskang <tkang@lat-tkang1.cb.careerbuilder.com careerbuilder.com>
2014-11-26 16:05:51 -0500

    gyroboy

--- lejos/src/EV3Tester.java
+++ lejos/src/EV3Tester.java
@@ -1,9 +1,15 @@
+import lejos.hardware.motor.Motor;
+import lejos.hardware.port.SensorPort;
+import lejos.hardware.sensor.EV3GyroSensor;
+import lejos.robotics.navigation.DifferentialPilot;
+
 
 public class EV3Tester {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		EV3GyroSensor gyro = new EV3GyroSensor(SensorPort.S2);
+//		Motor leftmotor = 
+		DifferentialPilot pilot;
 	}
 
 }
---
+++ lejos/src/GyroBoy.java
@@ -0,0 +1,8 @@
+
+public class GyroBoy {
+
+	public static void main(String[] args) {
+
+	}
+
+}
