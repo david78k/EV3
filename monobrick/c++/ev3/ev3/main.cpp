4023dd39adec1770cab08fac0de3c24d02957eb1
david78k <david78k@gmail.com>
2014-11-26 18:25:40 -0500

    main

---
+++ monobrick/c++/ev3/ev3/main.cpp
@@ -0,0 +1,16 @@
+#include <cstdlib>  
+#include <iostream>  
+#include <string>  
+#include <conio.h>
+//#include "brick.h"
+#include "nxt.h"
+
+using namespace std;
+
+Connection *connection = new Bluetooth();
+
+int main() {
+	printf("hello\n");
+	
+	return 0;
+}
