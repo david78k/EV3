0101dc50e61530a28091d2bae6821f489afa73d6
david78k <david78k@gmail.com>
2014-11-27 21:08:47 -0500

    sejway

--- ev3-lejos/src/Sejway.java
+++ ev3-lejos/src/Sejway.java
@@ -1,8 +1,11 @@
 import lejos.hardware.Button;
 import lejos.hardware.lcd.LCD;
 import lejos.hardware.motor.Motor;
+import lejos.hardware.motor.NXTMotor;
+import lejos.hardware.port.MotorPort;
 import lejos.hardware.port.SensorPort;
 import lejos.hardware.sensor.EV3GyroSensor;
+import lejos.robotics.EncoderMotor;
 import lejos.robotics.navigation.DifferentialPilot;
 
 //import lejos.nxt.*;
@@ -26,18 +29,29 @@ public class Sejway
     final int KD = 33;
     final int SCALE = 18;
 
-    // Global vars:
-    int offset;
-    int prev_error;
-    float int_error;
-	
 //    LightSensor ls;
     EV3GyroSensor gyro = new EV3GyroSensor(SensorPort.S2);
     private DifferentialPilot pilot = new DifferentialPilot(5.6, 9.25, Motor.A, Motor.D);
-    
+	EncoderMotor leftMotor = new NXTMotor(MotorPort.A); 
+	EncoderMotor rightMotor = new NXTMotor(MotorPort.D); 
+	
+	// Global vars:
+	int offset;
+	int prev_error;
+	float int_error;
+	
+	public static void main(String[] args) 
+	{
+		Sejway sej = new Sejway();
+//		sej.getBalancePos();
+		sej.pidControl();
+		sej.shutDown();
+	}
+	
     public Sejway() 
     {
 //        ls = new LightSensor(SensorPort.S2, true);
+    	gyro.reset();
     }
 	
     public void getBalancePos() 
@@ -47,19 +61,37 @@ public class Sejway
     	{
     		// NXTway must be balanced.
 //    		offset = ls.readNormalizedValue();
-//    		offset = gyro.getAngleMode();
     		LCD.clear();
     		LCD.drawInt(offset, 2, 4);
     		LCD.refresh();
     	}
     }
 	
+    /**
+	 * verified
+	 * average of 5 samples of angular velocity
+	 */
+	float gyroRate() {
+		float filter = 0;
+
+		// get 5 samples
+		float[] sample = new float[5];
+		int offset = 0;
+		gyro.getRateMode().fetchSample(sample, offset );
+//		gyro.getAngleMode().fetchSample(sample, offset );
+		for(int i = 0; i < 5; i ++)
+			filter += sample[i];
+//			filter = ev3.getAngularVelocity () + filter;
+		
+		return filter / 5f;
+	}
+	
     public void pidControl() 
     {
         while (!Button.ESCAPE.isDown()) 
         {
 //            int normVal = ls.readNormalizedValue();
-        	int normVal = 0;
+        	int normVal = (int) gyroRate();
 
             // Proportional Error:
             int error = normVal - offset;
@@ -84,13 +116,18 @@ public class Sejway
             int power = Math.abs(pid_val);
             power = 55 + (power * 45) / 100; // NORMALIZE POWER
 
-
+            leftMotor.setPower(power);
+            rightMotor.setPower(power);
             if (pid_val > 0) {
 //                MotorPort.B.controlMotor(power, BasicMotorPort.FORWARD);
 //                MotorPort.C.controlMotor(power, BasicMotorPort.FORWARD);
+            	leftMotor.forward();
+            	rightMotor.forward();
             } else {
 //                MotorPort.B.controlMotor(power, BasicMotorPort.BACKWARD);
 //                MotorPort.C.controlMotor(power, BasicMotorPort.BACKWARD);
+            	leftMotor.forward();
+            	rightMotor.forward();
             }
         }
     }
@@ -101,13 +138,7 @@ public class Sejway
 //        Motor.B.flt();
 //        Motor.C.flt();
 //        ls.setFloodlight(false);
-    }
-	
-    public static void main(String[] args) 
-    {
-        Sejway sej = new Sejway();
-        sej.getBalancePos();
-        sej.pidControl();
-        sej.shutDown();
+    	leftMotor.flt();
+    	rightMotor.flt();
     }
 }
