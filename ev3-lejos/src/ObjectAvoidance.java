afbf498c2c3ff226d74fb4b51ebeff8bcb692649
david78k <david78k@gmail.com>
2014-12-03 01:40:10 -0500

    run

--- ev3-lejos/src/ObjectAvoidance.java
+++ ev3-lejos/src/ObjectAvoidance.java
@@ -1,5 +1,6 @@
 import java.io.File;
 
+import lejos.hardware.Button;
 import lejos.hardware.motor.NXTMotor;
 import lejos.hardware.port.MotorPort;
 import lejos.hardware.port.SensorPort;
@@ -12,13 +13,16 @@ public class ObjectAvoidance {
 //	private static final boolean DEBUG = true;
 	private static final boolean DEBUG = false;
 
+	private static final int max_iter = 10;
+			
 	EV3UltrasonicSensor ultra = new EV3UltrasonicSensor(SensorPort.S4); // ultrasonic
 	EncoderMotor leftMotor = new NXTMotor(MotorPort.A); 
 	EncoderMotor rightMotor = new NXTMotor(MotorPort.D); 
 	File logfile = new File("/home/root/avoidance.log");
 	
 	public static void main(String[] args) {
-
+		ObjectAvoidance oa = new ObjectAvoidance();
+		oa.run();
 	}
 
 	public void run() {
@@ -26,15 +30,23 @@ public class ObjectAvoidance {
 		float dist = 0;
 		float[] sample = new float[1];
 		
-		while(true) {
+		int i = 0;
+		while(i++ < max_iter && !Button.ESCAPE.isDown()) {
 			speed = 50;
 			while(true) {
 				// motor rotation degree > 25
 				ultra.getDistanceMode().fetchSample(sample, 0);
 				dist = sample[0];
+				System.out.println(dist);
 				Delay.msDelay(200);
 				if(dist <= 30) break;
 			}
+			// object detected
+			speed = -30;
+			Delay.msDelay(2000);
+			speed = 0;
+			// steering = 30;
+			Delay.msDelay(2000);
 		}
 	}
 }
