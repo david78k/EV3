729fb7dee6edaff66bc4c6ae652bd4c6f13953dc
david78k <david78k@gmail.com>
2014-12-03 22:46:02 -0500

    MLP

---
+++ ev3-lejos/src/NeuralNetwork.java
@@ -0,0 +1,18 @@
+import lejos.utility.Matrix;
+
+public class NeuralNetwork {
+
+	private int nInputs = 2; // number of inputs
+	private int nOutputs = 5; // number of outputs
+	private float[] inputs = new float[nInputs];
+	private float[] outputs = new float[nOutputs];
+	private float w[] = new float[nInputs * nOutputs];
+	
+	public static void main(String[] args) {
+
+	}
+
+	public void train() {
+//		Matrix w = new Matrix(m, n);
+	}
+}
--- ev3-lejos/src/ObjectAvoidance.java
+++ ev3-lejos/src/ObjectAvoidance.java
@@ -22,9 +22,52 @@ public class ObjectAvoidance {
 	
 	public static void main(String[] args) {
 		ObjectAvoidance oa = new ObjectAvoidance();
-		oa.run();
+		oa.runNeuralNetwork();
+//		oa.run();
 	}
 
+	public void runNeuralNetwork() {
+		
+		
+		
+		int speed = 0;
+		float dist = 0;
+		float[] sample = new float[1];
+		
+		int i = 0;
+		while(i++ < max_iter && !Button.ESCAPE.isDown()) {
+			speed = 50;
+			// move straight
+			leftMotor.setPower(speed);
+			rightMotor.setPower(speed);
+			while(true) {
+				// motor rotation degree > 25
+				// in meter
+				ultra.getDistanceMode().fetchSample(sample, 0);
+				dist = sample[0];
+				Delay.msDelay(200);
+				if(dist <= 0.3) break;
+			}
+			// object detected
+			System.out.println(dist);
+			
+			// move back
+			speed = -30;
+			leftMotor.setPower(speed);
+			rightMotor.setPower(speed);
+			Delay.msDelay(2000);
+			
+			// move right
+			// steering = 30;
+			speed = -50;
+			leftMotor.setPower(speed);
+			speed = 50;
+			rightMotor.setPower(speed);
+			Delay.msDelay(2000);
+		}
+		Delay.msDelay(10000);
+	}
+	
 	public void run() {
 		int speed = 0;
 		float dist = 0;
